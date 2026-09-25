bbd285feecabfc412fae727c8e46bc3183383e80
EijiGorilla <emasuz@hotmail.com>
2026-09-25 09:06:10 +0800

    Updates

---
+++ assets/executeForTopCount-DjLAX9Wv.js
@@ -0,0 +1,1 @@
+import{dV as m}from"./index-D-ajXlTu.js";import{a as n}from"./queryTopFeatures-B2XbqLIt.js";import f from"./TopFeaturesQuery-Dh_zdCnr.js";async function i(o,t,r){const a=m(o);return(await n(a,f.from(t),{...r})).data.count}export{i as executeForTopCount};
---
+++ assets/projectMeshVertexPositions-ChMWtfoo.js
@@ -0,0 +1,1 @@
+import{c6 as s,ad as i,bu as n}from"./index-D-ajXlTu.js";import{q as a}from"./vertexSpaceConversion-COumG-KG.js";import"./vec4-cJ81CNVm.js";function u(t,r){const o=a(t,s.absolute);if(!o)return null;let e=o.position;return i(t.spatialReference,r)||(e=new Float64Array(o.position.length),n(o.position,t.spatialReference,0,e,r,0))?e:null}export{u as projectMeshVertexPositions};
